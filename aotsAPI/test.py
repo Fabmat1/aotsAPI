92f403b0313515432cf55bba83658da3082b9f94
Fabian Mattig <88104880+Fabmat1@users.noreply.github.com>
2023-08-02 11:36:15 +0200

    Unstable partial commit for SED and RV curves

--- aotsAPI/test.py
+++ aotsAPI/test.py
@@ -1,11 +1,16 @@
 import aotsAPI
-from api_calls import bulk_upload_spectra
+import astropy.io.fits as fits
+from api_calls import bulk_upload_spectra, bulk_download_spectra
 
 # aotsAPI.config_params["url"] = http://somealternativeurl.com # parameters may be modified like this
 
-response = bulk_upload_spectra([r"/path/to/file1",
-                                r"/path/to/file2",
-                                r"/path/to/file3",
-                                r"/path/to/file4"], 1)
+# response = bulk_upload_spectra([r"spec-0299-51671-05921.fits",
+#                                 r"spec-4040-55605-0538.fits",
+#                                 r"spec-4040-55605-05381.fits",
+#                                 r"spec-56389-HD134427N004207M01_sp08-158.fits",], 1)
+#
+# print(response.status_code, response.content)
 
-print(response)
+
+response = bulk_download_spectra(["LAMOST J134545.23-000641.6"], 1, "test.zip")
+print(response.status_code)
