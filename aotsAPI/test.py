23fbb0218b124c1cae69210d35185678345a2c04
Fabmat1 <fabmat1701@gmail.com>
2023-08-09 13:25:17 +0200

    Basic RV curve detail page & API Upload functionality

--- aotsAPI/test.py
+++ aotsAPI/test.py
@@ -1,11 +1,13 @@
 import aotsAPI
-from api_calls import bulk_upload_spectra
+from api_calls import bulk_upload_spectra, bulk_upload_rvcurves
 
 # aotsAPI.config_params["url"] = http://somealternativeurl.com # parameters may be modified like this
 
-response = bulk_upload_spectra([r"/path/to/file1",
-                                r"/path/to/file2",
-                                r"/path/to/file3",
-                                r"/path/to/file4"], 1)
+# response = bulk_upload_spectra([r"C:\Users\fabia\Downloads\spec-0348-51671-0043.fits",
+#                                 r"C:\Users\fabia\Downloads\spec-0348-51671-0043.fits",
+#                                 r"C:\Users\fabia\Downloads\spec-0348-51671-0043.fits",
+#                                 r"C:\Users\fabia\Downloads\spec-0348-51671-0043.fits"], 1)
 
-print(response)
+response = bulk_upload_rvcurves([r"C:\Users\fabia\PycharmProjects\RVVD\test.fits"], 1)
+
+print(response.content)
