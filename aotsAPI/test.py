3929987e9bb22aacdcbb2d6769a7bba2808644ca
Fabmat1 <fabmat1701@gmail.com>
2023-08-09 21:34:31 +0200

    Fixed RV curve list

--- aotsAPI/test.py
+++ aotsAPI/test.py
@@ -1,3 +1,5 @@
+import os
+
 import aotsAPI
 from api_calls import bulk_upload_spectra, bulk_upload_rvcurves
 
@@ -8,6 +10,21 @@ from api_calls import bulk_upload_spectra, bulk_upload_rvcurves
 #                                 r"C:\Users\fabia\Downloads\spec-0348-51671-0043.fits",
 #                                 r"C:\Users\fabia\Downloads\spec-0348-51671-0043.fits"], 1)
 
-response = bulk_upload_rvcurves([r"C:\Users\fabia\PycharmProjects\RVVD\test.fits"], 1)
+
+filelist = []
+
+for dir in os.listdir(r"C:\Users\fabia\PycharmProjects\RVVD\output"):
+    if len(filelist) > 50:
+        response = bulk_upload_rvcurves(filelist, 1)
+        print(response.status_code)
+        print(response.content)
+        filelist = []
+
+    for file in os.listdir("C:\\Users\\fabia\\PycharmProjects\\RVVD\\output\\" + dir):
+        if file.endswith(".fits"):
+            filelist.append("C:\\Users\\fabia\\PycharmProjects\\RVVD\\output\\" + dir + "\\"+file)
+
+
+
 
 print(response.content)
